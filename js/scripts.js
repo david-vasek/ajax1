a6fc6c4805b4897dec39eb31735cc3a4b0616429
David Vasek <david.vasek51@gmail.com>
2021-05-28 11:13:00 -0500

    refactored the code to maintain currentCategory and update button

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,98 @@
-'use strict'
+'use strict';
 
-// XHR Example
 // const request = new XMLHttpRequest();
-// request.onreadystatechange = function() {
+// request.onreadystatechange = function () {
 //     console.log(this);
 //     if (this.readyState === 4) {
-//         console.log("Response is: ", this.response);
+//         console.log("XHR Response: ", this.response);
 //     }
 // }
 // request.open('GET', 'https://api.chucknorris.io/jokes/random?category=dev');
 // request.send();
 
-function fetchTheQuote(category) {
-    fetch(
-        `https://api.chucknorris.io/jokes/random?category=${category}`
-    ).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        updateQuote(data);
-    });
-}
 
-function fetchTheCategories() {
-    fetch('https://api.chucknorris.io/jokes/categories')
-        .then(function (response) {
+document.addEventListener('DOMContentLoaded', function () {
+    const generateQuote = document.querySelector('#getQuote');
+    const chuckQuote = document.querySelector('#chuckQuote');
+    const defaultCategory = 'dev';
+    let currentCategory = defaultCategory;
+
+    function fetchTheQuote(category) {
+        fetch(
+            `https://api.chucknorris.io/jokes/random?category=${category}`)
+            .then(function (response) {
+                return response.json();
+        })
+            .then(function (data) {
+                updateQuote(data);
+        });
+    }
+    function updateQuote(data) {
+        const theQuote = data.value;
+        chuckQuote.innerText = theQuote;
+    }
+
+    function fetchTheCategories() {
+        fetch(
+            'https://api.chucknorris.io/jokes/categories'
+        ).then(function (response) {
             return response.json();
-    })
-    .then(function(data) {
-        console.log("category data is: ", data);
-        updateCategories(data);
-    });
-}
+        }).then(function (data) {
+            updateCategories(data);
+        });
+    }
 
-const chuckQuote = document.querySelector('#chuckQuote');
-const quoteButton = document.querySelector('#getQuote');
-quoteButton.addEventListener('click', function() {
-    fetchTheQuote();
+
+    function updateCategories(categoryData) {
+        const filteredList = categoryData.filter(function(category) {
+            if (category !== 'explicit' && category !== 'political' && category !== 'sport') {
+                return category;
+            }
+        })
+
+        
+        const categoryListForm = document.querySelector('#categoryList');
+        const selectElement = document.createElement('select');
+
+        filteredList.forEach(function (category) {
+            const categoryOptionElement = document.createElement('option');
+            categoryOptionElement.value = category;
+            categoryOptionElement.text = category;
+            if (category === currentCategory) {
+                categoryOptionElement.setAttribute('selected', true);
+            }
+            selectElement.appendChild(categoryOptionElement);
+        });
+
+        categoryListForm.append(selectElement);
+
+        selectElement.addEventListener('change', function (event) {
+            const newCategoryName = event.target.value;
+            currentCategory = newCategoryName;
+            fetchTheQuote(newCategoryName);
+        })
+    }
+
+    generateQuote.addEventListener('click', function () {
+        console.log("current category: ", currentCategory );
+        fetchTheQuote(currentCategory);
+    });
+
+    fetchTheQuote(currentCategory);
+    fetchTheCategories();
 });
 
 
-function updateCategories(categoryData) {
-    const categoryListForm = document.querySelector("#categoryList");
-    const selectElement = document.createElement('select');
-    categoryData.forEach(function (category) {
-        const categoryOptionElement = document.createElement('option');
-        categoryOptionElement.value = category;
-        categoryOptionElement.text = category;
-        selectElement.appendChild(categoryOptionElement);
-    });
-    categoryListForm.appendChild(selectElement);
 
-    selectElement.addEventListener('change', function(event) {
-        console.log("the event is", event.target.value);
-        const categoryName = event.target.value;
-        fetchTheQuote(categoryName);
-    });
-}
 
-function updateQuote(dataFromFetch) {
-    const theQuote = dataFromFetch.value;
-    console.log(theQuote);
-    chuckQuote.innerText = theQuote;
-}
 
-fetchTheQuote('dev');
-fetchTheCategories();
+
+
+
+
+
+
+
+
+
+
